1e0a054c9eb64695187f9622505e575b695f7270
Dan <danielmi@unity3d.com>
2019-02-13 13:23:30 -0800

    added remoting, hooked up LWRP asset in graphics settings

---
+++ Assets/ARFoundationSamples/Scripts/Remoting/PlaceWIthRemote.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceWIthRemote : MonoBehaviour
+{
+
+    [SerializeField] GameObject hitObjectPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hitInfo;
+            Ray cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(cameraRay, out hitInfo))
+            {
+                Debug.Log(hitInfo.transform.name);
+                Instantiate(hitObjectPrefab, hitInfo.point, Quaternion.identity);
+            }
+        }
+    }
+}
