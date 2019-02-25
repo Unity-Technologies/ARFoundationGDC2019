743ab0cc34b757a2ff36133c0f0511ceb7abcf2d
Andy Touch <andyt@unity3d.com>
2019-02-25 19:30:27 +0100

    Big push of Timeline and Hologram Setup
    Recommended to use 'Scene_Dev_Timeline'

---
+++ Assets/Effects/Scripts/EnvironmentSliceController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class EnvironmentSliceController : MonoBehaviour
+{
+    
+    void Update()
+    {
+        Shader.SetGlobalVector("_Slice_Transform", transform.position);
+    }
+}
---
+++ Assets/Effects/Scripts/HologramBlendController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class HologramBlendController : MonoBehaviour
+{
+    public float blendValue;
+    private int blendValueID;
+
+    public Texture2D effectMap;
+    private int effectMapID;
+
+    public float effectMapTile;
+    private int effectMapTileID;
+
+    public float effectSpread;
+    private int effectSpreadID;
+
+    [ColorUsage(true, true)] public Color edgeColor;
+    private int edgeColorID;
+
+
+    void OnEnable()
+    {
+        blendValueID = Shader.PropertyToID("_Effect_Blend");
+        effectMapID = Shader.PropertyToID("_Effect_Map");
+        effectMapTileID = Shader.PropertyToID("_Effect_MapTile");
+        effectSpreadID = Shader.PropertyToID("_Effect_Spread");
+        edgeColorID = Shader.PropertyToID("_Edge_Color");
+    }
+
+
+    void Update()
+    {
+       Shader.SetGlobalFloat(blendValueID, blendValue);
+       Shader.SetGlobalTexture(effectMapID, effectMap);
+       Shader.SetGlobalFloat(effectMapTileID, effectMapTile);
+       Shader.SetGlobalFloat(effectSpreadID, effectSpread);
+       Shader.SetGlobalColor(edgeColorID, edgeColor);
+    }
+
+}
