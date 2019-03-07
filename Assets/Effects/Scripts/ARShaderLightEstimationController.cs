1ea40bf2887416ff71b7e02021e613c3703ef1f4
Andy Touch <andyt@unity3d.com>
2019-03-07 15:26:10 +0100

    Re-setup Plant SG, added Mask Maps, added AR Shader light Estimation Controller be Global

---
+++ Assets/Effects/Scripts/ARShaderLightEstimationController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class ARShaderLightEstimationController : MonoBehaviour
+{
+
+    [Header("References")]
+    LightEstimation lightEstimation;
+    public Light directionalLight;
+
+    [Header("Editor Testing (Ignored at Runtime)")]
+    public float editorEmissionValue;
+    public Color editorColorValue;
+
+    private int arLightEmissionID;
+    private int arLightColorID;
+
+    void OnEnable()
+    {
+        arLightEmissionID = Shader.PropertyToID("_AR_LightEmission");
+        arLightColorID = Shader.PropertyToID("_AR_LightColor");
+
+        lightEstimation = FindObjectOfType<LightEstimation>();
+    }
+
+    void Update()
+    {
+         if(!Application.isPlaying)
+        {
+           Shader.SetGlobalFloat(arLightEmissionID, editorEmissionValue);
+           Shader.SetGlobalColor(arLightColorID, editorColorValue);
+
+        } else {
+
+            Shader.SetGlobalFloat(arLightEmissionID, lightEstimation.brightness.Value);
+            Shader.SetGlobalColor(arLightColorID, directionalLight.color);
+            
+        }
+    }
+}
